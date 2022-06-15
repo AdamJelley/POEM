c670844fe98e18a9cfc2dea315a4fb74b50be9ac
Adam Jelley <adjelley@gmail.com>
2022-06-15 12:38:13 +0100

    Refactor data generator
    Write function for generation process

--- generate_trajectories.py
+++ generate_trajectories.py
@@ -12,166 +12,191 @@ from utils import device
 
 
 # Parse arguments
+def parse_generation_args():
+    parser = argparse.ArgumentParser(description="Data generation arguments")
+    parser.add_argument(
+        "--env", required=True, help="name of the environment to be run (REQUIRED)"
+    )
+    parser.add_argument(
+        "--model", required=True, help="name of the trained model (REQUIRED)"
+    )
+    parser.add_argument("--seed", type=int, default=0, help="random seed (default: 0)")
+    parser.add_argument(
+        "--shift",
+        type=int,
+        default=0,
+        help="number of times the environment is reset at the beginning (default: 0)",
+    )
+    parser.add_argument(
+        "--argmax",
+        action="store_true",
+        default=False,
+        help="select the action with highest probability (default: False)",
+    )
+    parser.add_argument(
+        "--pause",
+        type=float,
+        default=0.1,
+        help="pause duration between two consequent actions of the agent (default: 0.1)",
+    )
+    parser.add_argument(
+        "--gif",
+        type=str,
+        default=None,
+        help="store output as gif with the given filename",
+    )
+    parser.add_argument(
+        "--episodes", type=int, default=10, help="number of episodes to visualize"
+    )
+    parser.add_argument(
+        "--memory", action="store_true", default=False, help="add a LSTM to the model"
+    )
+    parser.add_argument(
+        "--text", action="store_true", default=False, help="add a GRU to the model"
+    )
+    parser.add_argument(
+        "--render", action="store_true", default=False, help="render data generation"
+    )
+    parser.add_argument(
+        "--save_data",
+        action="store_true",
+        default=False,
+        help="save generated data to file",
+    )
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        default=None,
+        help="Name of dataset to store (default: env-model-epsiodes)",
+    )
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--env", required=True, help="name of the environment to be run (REQUIRED)"
-)
-parser.add_argument(
-    "--model", required=True, help="name of the trained model (REQUIRED)"
-)
-parser.add_argument("--seed", type=int, default=0, help="random seed (default: 0)")
-parser.add_argument(
-    "--shift",
-    type=int,
-    default=0,
-    help="number of times the environment is reset at the beginning (default: 0)",
-)
-parser.add_argument(
-    "--argmax",
-    action="store_true",
-    default=False,
-    help="select the action with highest probability (default: False)",
-)
-parser.add_argument(
-    "--pause",
-    type=float,
-    default=0.1,
-    help="pause duration between two consequent actions of the agent (default: 0.1)",
-)
-parser.add_argument(
-    "--gif", type=str, default=None, help="store output as gif with the given filename"
-)
-parser.add_argument(
-    "--episodes", type=int, default=10, help="number of episodes to visualize"
-)
-parser.add_argument(
-    "--memory", action="store_true", default=False, help="add a LSTM to the model"
-)
-parser.add_argument(
-    "--text", action="store_true", default=False, help="add a GRU to the model"
-)
-parser.add_argument(
-    "--dataset_name",
-    type=str,
-    default="Test",
-    help="Name of dataset to store (default: env-model-epsiodes)",
-)
+    args = parser.parse_args()
+    args.dataset_name = f"{args.env}-{args.model}-{args.episodes}"
+    return args
 
-args = parser.parse_args()
-args.dataset_name = f"{args.env}-{args.model}-{args.episodes}"
 
-# Set seed for all randomness sources
+def generate_data(
+    env,
+    agent,
+    episodes,
+    render=False,
+    gif=False,
+    save_data=False,
+    dataset_name="test_data",
+):
 
-utils.seed(args.seed)
+    dataset = {}
 
-# Set device
+    if gif:
+        from array2gif import write_gif
 
-print(f"Device: {device}\n")
+        frames = []
 
-# Load environment
+    for episode in range(episodes):
+        obs = env.reset()
+        step = 0
+        trajectory = {}
 
-env = utils.make_env(args.env, args.seed)
-for _ in range(args.shift):
-    env.reset()
-print("Environment loaded\n")
+        while True:
+            if render:
+                env.render("human")
 
-# Load agent
+            if gif:
+                frames.append(numpy.moveaxis(env.render("rgb_array"), 2, 0))
 
-model_dir = utils.get_model_dir(args.model, storage_dir="minigrid-rl-starter")
+            obs["pixels"] = numpy.moveaxis(env.render("rgb_array"), 2, 0)
+            obs["partial_pixels"] = numpy.moveaxis(
+                env.get_obs_render(obs["partial_image"], tile_size=8), 2, 0
+            )
+            location = env.agent_pos
+            action = agent.get_action(obs)
+            new_obs, reward, done, _ = env.step(action)
 
-agent = utils.Agent(
-    env.observation_space,
-    env.action_space,
-    model_dir,
-    argmax=args.argmax,
-    use_memory=args.memory,
-    use_text=args.text,
-)
-print("Agent loaded\n")
+            new_obs["pixels"] = numpy.moveaxis(env.render("rgb_array"), 2, 0)
+            new_obs["partial_pixels"] = numpy.moveaxis(
+                env.get_obs_render(new_obs["partial_image"], tile_size=8), 2, 0
+            )
+            new_location = env.agent_pos
 
-# Run the agent
+            transition = {
+                "obs": obs,
+                "action": action,
+                "reward": reward,
+                "new_obs": new_obs,
+                "done": done,
+                "location": location,
+                "new_location": new_location,
+                "direction": obs["direction"],
+                "new_direction": new_obs["direction"],
+            }
 
-if args.gif:
-    from array2gif import write_gif
+            trajectory[step] = transition
 
-    frames = []
+            obs = new_obs
+            agent.analyze_feedback(reward, done)
+            step += 1
 
-# Create a window to view the environment
-env.render("human")
+            if done or render and env.window.closed:
+                break
 
-dataset = {}
-
-for episode in range(args.episodes):
-    obs = env.reset()
-    step = 0
-    trajectory = {}
-
-    while True:
-        env.render("human")
-
-        if args.gif:
-            frames.append(numpy.moveaxis(env.render("rgb_array"), 2, 0))
-
-        obs["pixels"] = numpy.moveaxis(env.render("rgb_array"), 2, 0)
-        obs["partial_pixels"] = numpy.moveaxis(
-            env.get_obs_render(obs["partial_image"], tile_size=8), 2, 0
-        )
-        location = env.agent_pos
-        action = agent.get_action(obs)
-        new_obs, reward, done, _ = env.step(action)
-
-        new_obs["pixels"] = numpy.moveaxis(env.render("rgb_array"), 2, 0)
-        new_obs["partial_pixels"] = numpy.moveaxis(
-            env.get_obs_render(new_obs["partial_image"], tile_size=8), 2, 0
-        )
-        new_location = env.agent_pos
-
-        transition = {
-            "obs": obs,
-            "action": action,
-            "reward": reward,
-            "new_obs": new_obs,
-            "done": done,
-            "location": location,
-            "new_location": new_location,
-            "direction": obs["direction"],
-            "new_direction": new_obs["direction"],
-        }
-
-        trajectory[step] = transition
-
-        # new_obs["image"][env.agent_pos[0], env.agent_pos[1], 2] = 0
-        # if numpy.allclose(new_obs["image"], obs["image"]):
-        #     print("Ivariant")
-        # else:
-        #     print("rotated")
-        # print(obs["image"], env.agent_pos)
-        # print(env.agent_pos, env.agent_dir)
-        # if episode == 0:
-        #     numpy.save("test.npz", obs["image"])
-        # plt.imshow(obs)
-        # plt.show()  # show the figure, non-blocking
-        # _ = input("Press [enter] to continue.")  # wait for input from the user
-        # plt.close()  # close the figure to show the next one.
-        obs = new_obs
-        agent.analyze_feedback(reward, done)
-        step += 1
-
-        if done or env.window.closed:
+        dataset[episode] = trajectory
+        if render and env.window.closed:
             break
 
-    dataset[episode] = trajectory
-    if env.window.closed:
-        break
+    if gif:
+        print("Saving gif... ", end="")
+        write_gif(numpy.array(frames), args.gif + ".gif", fps=1 / args.pause)
+        print("Done.")
 
-if not os.path.isdir("datasets"):
-    os.mkdir("datasets")
-with open(f"datasets/{args.dataset_name}.pickle", "wb") as handle:
-    pickle.dump(dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
-print(dataset)
+    if save_data:
+        if not os.path.isdir("datasets"):
+            os.mkdir("datasets")
+        with open(f"datasets/{dataset_name}.pickle", "wb") as handle:
+            pickle.dump(dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-if args.gif:
-    print("Saving gif... ", end="")
-    write_gif(numpy.array(frames), args.gif + ".gif", fps=1 / args.pause)
-    print("Done.")
+    return dataset
+
+
+if __name__ == "__main__":
+    args = parse_generation_args()
+
+    # Set seed for all randomness sources
+    utils.seed(args.seed)
+
+    # Set device
+
+    print(f"Device: {device}\n")
+
+    # Load environment
+
+    env = utils.make_env(args.env, args.seed)
+    for _ in range(args.shift):
+        env.reset()
+    print("Environment loaded\n")
+
+    # Load agent
+
+    model_dir = utils.get_model_dir(args.model, storage_dir="minigrid-rl-starter")
+
+    agent = utils.Agent(
+        env.observation_space,
+        env.action_space,
+        model_dir,
+        argmax=args.argmax,
+        use_memory=args.memory,
+        use_text=args.text,
+    )
+    print("Agent loaded\n")
+
+    # Run the agent
+    dataset = generate_data(
+        env,
+        agent,
+        args.episodes,
+        render=args.render,
+        gif=args.gif,
+        save_data=args.save_data,
+        dataset_name=args.dataset_name,
+    )
+
+    print(dataset)
